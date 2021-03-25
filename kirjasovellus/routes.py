02c93e5f7a975506d7bdafec6d3624839d967936
Oskari-Juvakka <oskari.juvakka@gmail.com>
2021-03-25 16:06:36 +0200

    worked with templates

--- kirjasovellus/routes.py
+++ kirjasovellus/routes.py
@@ -14,10 +14,13 @@ def redirect_to_register():
 @app.route("/register", methods=["POST"])
 def register():
     #todo syötteen validiointi?
-    username = request.form["username"]
-    password = request.form["password"]
-    users.register(username, password)
-    return redirect("/login")
+    username = request.form["username1"]
+    password = request.form["password1"]
+    register_ok = users.register(username, password)
+    register_ok_info = "Jokin meni pieleen. Voi olla, että käyttäjätunnus on varattu. Ongelman jatkuessa kannattaa koittaa jotain muuta käyttäjätunnusta."
+    if register_ok:
+        register_ok_info = "Rekisteröityminen onnistui, voit nyt kirjautua"
+    return render_template("index.html", register_ok=register_ok_info)
 
 @app.route("/login")
 def login():
@@ -25,8 +28,8 @@ def login():
 
 @app.route("/login", methods=["POST"])
 def login_result():
-    username = request.form["username"]
-    password = request.form["password"]
+    username = request.form["username2"]
+    password = request.form["password2"]
     if users.login(username, password):
         session["username"] = username
         session["csrf_token"] = urandom(16).hex()
@@ -37,3 +40,12 @@ def logout():
     del session["username"]
     del session["csrf_token"]
     return redirect("/")
+
+# TODO
+@app.route("/bookshelf")
+def bookshelf():
+    return render_template("bookshelf.html")
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
--- kirjasovellus/users.py
+++ kirjasovellus/users.py
@@ -1,11 +1,16 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from db import db
+from sqlalchemy import exc
 
 def register(username: str, password: str):
-    password_value = generate_password_hash(password)
-    sql = "INSERT INTO users (username, password) VALUES (:username,:password)"
-    db.session.execute(sql, {"username":username,"password":password_value})
-    db.session.commit()
+    try:
+        password_value = generate_password_hash(password)
+        sql = "INSERT INTO users (username, password) VALUES (:username,:password)"
+        db.session.execute(sql, {"username":username,"password":password_value})
+        db.session.commit()
+        return True
+    except exc.SQLAlchemyError:
+        return False
 
 def login(username: str, password: str):
     sql = "SELECT password FROM users WHERE username=:username"
