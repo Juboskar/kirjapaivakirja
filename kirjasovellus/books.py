916cf87e66ed61ee959539613b86dc92859a7125
Oskari-Juvakka <oskari.juvakka@gmail.com>
2021-03-30 15:03:05 +0300

    worked with searching books

--- kirjasovellus/books.py
+++ kirjasovellus/books.py
@@ -9,3 +9,31 @@ def add_new_book(title: str, author: str, genre: str, isbn:str, pages:int):
         return True
     except exc.SQLAlchemyError:
         return False
+
+def find_books(query:str, selection:int):
+    try:
+        books = []
+        if "2" in selection or "1" in selection: 
+            sql = "SELECT * FROM books WHERE title ILIKE :query"
+            result = db.session.execute(sql, {"query":"%"+query+"%"})
+            books.extend(result.fetchall())
+
+        if "3" in selection or "1" in selection:  
+            sql = "SELECT * FROM books WHERE author ILIKE :query"
+            result = db.session.execute(sql, {"query":"%"+query+"%"})
+            books.extend(result.fetchall())
+
+        if "4" in selection or "1" in selection:  
+            sql = "SELECT * FROM books WHERE genre ILIKE :query"
+            result = db.session.execute(sql, {"query":"%"+query+"%"})
+            books.extend(result.fetchall())
+
+        if "5" in selection or "1" in selection:  
+            sql = "SELECT * FROM books WHERE isbn ILIKE :query"
+            result = db.session.execute(sql, {"query":"%"+query+"%"})
+            books.extend(result.fetchall())
+        
+        return list(set(books))
+
+    except exc.SQLAlchemyError:
+        return []
--- kirjasovellus/routes.py
+++ kirjasovellus/routes.py
@@ -18,14 +18,15 @@ def register():
     username = request.form["username1"]
     password = request.form["password1"]
     register_ok = users.register(username, password)
-    register_ok_info = "Jokin meni pieleen. Voi olla, että käyttäjätunnus on varattu. Ongelman jatkuessa kannattaa koittaa jotain muuta käyttäjätunnusta."
+    register_ok_info = "Jokin meni pieleen. Voi olla, että käyttäjätunnus on varattu.\
+        Ongelman jatkuessa kannattaa koittaa jotain muuta käyttäjätunnusta."
     if register_ok:
         register_ok_info = "Rekisteröityminen onnistui, voit nyt kirjautua"
     return render_template("index.html", register_status=register_ok_info)
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return redirect("/")
 
 @app.route("/login", methods=["POST"])
 def login_result():
@@ -49,8 +50,14 @@ def logout():
 def bookshelf():
     return render_template("bookshelf.html")
 
-@app.route("/addbook", methods=["POST"])
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+@app.route("/search/addbook", methods=["POST"])
 def addbook():
+    if session["csrf_token"] != request.form["csrf_token"]:
+        abort(403)
     title = request.form["title"]
     author = request.form["author"]
     genre = request.form["genre"]
@@ -59,8 +66,12 @@ def addbook():
     if books.add_new_book(title, author, genre, isbn, pages):
         return redirect("/search")
     else:
-        return render_template("search.html", info = "Jokin meni pieleen")
+        return render_template("search.html", info="Jokin meni pieleen")
 
-@app.route("/search")
-def search():
-    return render_template("search.html")
+@app.route("/search/findbooks")
+def findbooks():
+    selection = request.args.getlist("search_term")
+    print(selection)
+    query = request.args["query"]
+    result_books = books.find_books(query, selection)
+    return render_template("search.html", books = result_books[:30])
