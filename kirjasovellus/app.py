e307d8ddc818c83fc4c789c5637a892d2f821ede
Oskari-Juvakka <oskari.juvakka@gmail.com>
2021-04-19 16:41:25 +0300

    timeout

--- kirjasovellus/app.py
+++ kirjasovellus/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask import Flask
 from os import getenv
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
+
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=15)
 
 import routes
