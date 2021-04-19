7bf8a33676eabf077d5df124095459978bc60538
Oskari-Juvakka <oskari.juvakka@gmail.com>
2021-04-19 16:22:01 +0300

    reorganise

---
+++ schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
+CREATE TABLE books (id SERIAL PRIMARY KEY, title TEXT, author TEXT, genre TEXT, isbn TEXT UNIQUE, pages INTEGER);
+CREATE TABLE bookshelf_books (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users ON DELETE CASCADE, book_id INTEGER REFERENCES books ON DELETE CASCADE, progress INTEGER, update_date TIMESTAMP);
+CREATE TABLE ratings (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, book_id INTEGER REFERENCES books, star_rating INTEGER, review TEXT, rating_date TIMESTAMP);
